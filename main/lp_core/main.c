02a6cf4295c21b61e292cc69ffa19f42f8750e40
Michal Kubascik <michal.kubascik26799@gmail.com>
2026-06-05 16:47:46 +0200

    init

---
+++ main/lp_core/main.c
@@ -0,0 +1,7 @@
+
+int main (void)
+{
+    //TODO: read the data from BME690 over LP_I2C and classify it using the model 
+    while (1) {
+    }
+}
---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Hello ESP32-C6 from lp_enose\n");
+
+    //TODO: train the model on embedded csv data
+    
+    while (1) {
+    }
+}
